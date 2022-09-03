b1718bc09c84f055ffd43c19a7ef557244a1d1a9
Anna Leszka <ania.lalala97@gmail.com>
2022-09-03 11:50:07 +0200

    homework 7

--- src/services/contacts-api.js
+++ src/services/contacts-api.js
@@ -4,18 +4,15 @@ axios.defaults.baseURL = 'https://630fca12498924524a95b62d.mockapi.io'
 
 export async function fetchContacts() {
     const { data } = await axios.get('/contacts');
-    console.log('fetchContacts', data)
     return data;
-}
+};
 
 export async function addContact(contact) {
     const { data } = await axios.post('/contacts', contact);
-    console.log('addContact', data)
      return data;
-}
+};
 
 export async function deleteContact(id) {
-    const { data } = await axios.delete(`/contacts/${id}`)
-    console.log('deleteContact', data)
+    await axios.delete(`/contacts/${id}`)
     return id;
-}
+};
